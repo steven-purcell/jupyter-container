bab0c2833687eec25f0c3cb94801dfff9dd98797
steven <steven.ray.purcell@gmail.com>
2019-07-21 16:25:52 -0500

    update build and run.

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,4 +2,4 @@
 
 docker build --tag=jupyter .
 
-docker run -p 8888:8888 -v /Users/stevenpurcell/notebooks:/home/notebooks --restart unless-stopped jupyter
+nohup docker run -p 8888:8888 -v /Users/stevenpurcell/notebooks:/home/notebooks --restart unless-stopped jupyter &
